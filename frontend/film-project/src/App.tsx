4ae40e4816a658b0ba57d87eab534c0ad9c822e2
Archie <archie.calvert@outlook.com>
2024-11-09 16:44:34 +0000

    Created nodejs project

---
+++ frontend/film-project/src/App.tsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+
+import IndexPage from "@/pages/index";
+import DocsPage from "@/pages/docs";
+import PricingPage from "@/pages/pricing";
+import BlogPage from "@/pages/blog";
+import AboutPage from "@/pages/about";
+
+function App() {
+  return (
+    <h1>Hello</h1>
+  );
+}
+
+export default App;
