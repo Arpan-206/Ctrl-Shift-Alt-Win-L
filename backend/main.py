3f3484ca4dec7b229088968719d967c8ab190e4d
Arpan Pandey <arpanpandey.aps@gmail.com>
2024-11-09 12:57:37 +0000

    DB working baby

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
-
+from db import *
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
