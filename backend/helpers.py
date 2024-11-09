71afc62ad0469471d359e00ba4b5d1e796a02e75
Hufsa Haq <hufsahaq@gmail.com>
2024-11-09 17:14:57 +0000

    fix create_movie_from_imdb_id

--- backend/helpers.py
+++ backend/helpers.py
@@ -7,28 +7,25 @@ import requests
 load_dotenv()
 
 def create_movie(session: Session, movie: Movie):
-    movie = Movie.from_orm(movie)
     session.add(movie)
     session.commit()
     session.refresh(movie)
     return movie
 
 def get_movies_by_user_id(session: Session, user_id: str):
-    movies = session.exec(select(Movie).where(Movie.user_id == user_id)).all()
-    return movies
+    return session.exec(select(Movie).where(Movie.user_id == user_id)).all()
 
 def get_movie_by_id(session: Session, movie_id: str):
-    movie = session.get(Movie, movie_id)
-    return movie
+    return session.get(Movie, movie_id)
 
 def update_movie(session: Session, movie_id: str, movie: Movie):
-    movie = session.get(Movie, movie_id)
-    movie = Movie.from_orm(movie)
-    movie.update_from_dict(movie.dict())
-    session.add(movie)
-    session.commit()
-    session.refresh(movie)
-    return movie
+    existing_movie = session.get(Movie, movie_id)
+    if existing_movie:
+        existing_movie.update_from_dict(movie.dict())
+        session.commit()
+        session.refresh(existing_movie)
+        return existing_movie
+    return None
 
 def get_movie_details(imdb_id: str, session: Session):
     url = f"http://www.omdbapi.com/?i={imdb_id}&apikey={os.getenv('OMDB_API_KEY')}"
@@ -45,7 +42,7 @@ def get_movie_details(imdb_id: str, session: Session):
     else:
         print(f"Movie with IMDb ID {imdb_id} not found.")
         return None
-    
+
 def create_movie_from_imdb_id(imdb_id: str, user_id: str, session: Session):
     movie_details = get_movie_details(imdb_id, session)
     if movie_details:
@@ -58,5 +55,4 @@ def create_movie_from_imdb_id(imdb_id: str, user_id: str, session: Session):
             user_id=user_id,
         )
         return create_movie(session, movie)
-    else:
-        return None
+    return None
